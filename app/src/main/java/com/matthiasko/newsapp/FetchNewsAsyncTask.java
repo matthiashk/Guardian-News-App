45a061476d11e7e527a205451f2e767fd11eaa78
Matthias H. Ko <matthiashk@gmail.com>
2016-08-15 11:29:54 -0500

    Added empty asynctask and newsadapter classes Added NewsItem object and properties

---
+++ app/src/main/java/com/matthiasko/newsapp/FetchNewsAsyncTask.java
@@ -0,0 +1,7 @@
+package com.matthiasko.newsapp;
+
+/**
+ * Created by matthiasko on 8/15/16.
+ */
+public class FetchNewsAsyncTask {
+}
---
+++ app/src/main/java/com/matthiasko/newsapp/NewsItem.java
@@ -0,0 +1,55 @@
+package com.matthiasko.newsapp;
+
+import java.util.Date;
+
+/**
+ * Created by matthiasko on 8/15/16.
+ */
+public class NewsItem {
+
+    String id;
+    String sectionName;
+    Date publicationDate;
+    String title;
+    String webUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+}
